0a4da4ab6eb59416c80e7eaf25a9f4ce2711ffa9
Samuel Brasileiro <samuelbsantosn@gmail.com>
2020-03-17 11:48:40 -0300

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Samuel Brasileiro on 17/03/20.
+//  Copyright © 2020 Samuel Brasileiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
